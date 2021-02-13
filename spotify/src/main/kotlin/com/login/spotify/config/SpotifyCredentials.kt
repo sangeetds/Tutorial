eb04bbde4a69648e1bc58e8579bad2c12055d293
sangeetds <sangeetnarayands@gmail.com>
2021-02-13 12:46:23 +0530

    Spotify API working.

---
+++ spotify/src/main/kotlin/com/login/spotify/config/SpotifyCredentials.kt
@@ -0,0 +1,8 @@
+package com.login.spotify.config
+
+interface SpotifyCredentials {
+    companion object {
+        const val spotifyClientId = "608e8750aa7d4c3abbf82e12fc195210"
+        const val spotifyClientSecret = "9fef286d66464886b6619c5bcc493852"
+    }
+}
